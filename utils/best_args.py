a324bc526395a7be63a269c690e225ef3ab68bcc
Huyen Trang <bhtrang1109@gmail.com>
2024-05-17 03:42:55 +0700

    Update best_args.py

--- utils/best_args.py
+++ utils/best_args.py
@@ -1,43 +1,6 @@
 best_args = {
     'fl_digits': {
-        'feddf': {
-            -1: {
-                'communication_epoch': 40,
-                'local_lr': 0.001,
-                'public_lr': 0.001,
-                'local_epoch': 20,
-                'public_epoch': 1,
-                'public_batch_size': 256,
-                'local_batch_size': 128,
-            },
-        },
-        'fccl': {
-            -1: {
-                'communication_epoch': 40,
-                'local_lr': 0.001,
-                'public_lr': 0.001,
-                'local_epoch': 20,
-                'public_epoch': 1,
-                'public_batch_size': 256,
-                'local_batch_size': 128,
-                'off_diag_weight': 0.0051,
 
-            },
-        },
-        'fcclplus': {
-            -1: {
-                'communication_epoch': 40,
-                'local_lr': 0.001,
-                'public_lr': 0.001,
-                'local_epoch': 20,
-                'public_epoch': 1,
-                'public_batch_size': 256,
-                'local_batch_size': 128,
-                'temp': 0.02,
-                'dis_power': 3,
-                'local_dis_power': 3
-            },
-        },
         'fedavg': {
                 'local_lr': 0.01,
                 'local_batch_size': 64,
@@ -62,44 +25,7 @@ best_args = {
         }
     },
     'fl_officecaltech': {
-        'feddf': {
-            -1: {
-                'communication_epoch': 40,
-                'local_lr': 0.001,
-                'public_lr': 0.001,
-                'local_epoch': 20,
-                'public_epoch': 1,
-                'public_batch_size': 256,
-                'local_batch_size': 128,
-            },
-        },
-        'fccl': {
-            -1: {
-                'communication_epoch': 40,
-                'local_lr': 0.001,
-                'public_lr': 0.001,
-                'local_epoch': 20,
-                'public_epoch': 1,
-                'public_batch_size': 256,
-                'local_batch_size': 128,
-                'off_diag_weight': 0.0051,
 
-            },
-        },
-        'fcclplus': {
-            -1: {
-                'communication_epoch': 40,
-                'local_lr': 0.001,
-                'public_lr': 0.001,
-                'local_epoch': 20,
-                'public_epoch': 1,
-                'public_batch_size': 256,
-                'local_batch_size': 128,
-                'temp': 0.02,
-                'dis_power': 3,
-                'local_dis_power': 3
-            },
-        },
         'fedavg': {
             'local_lr': 0.01,
             'local_batch_size': 64,
@@ -123,6 +49,10 @@ best_args = {
         }
     },
     
+
+    },
+    
+
 
     
 }
