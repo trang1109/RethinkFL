9263c3f08eb6a7d8cee9ec72e77de3bef94382f6
Huyen Trang <bhtrang1109@gmail.com>
2024-05-17 03:02:50 +0700

    Update best_args.py

--- utils/best_args.py
+++ utils/best_args.py
@@ -1,6 +1,43 @@
 best_args = {
     'fl_digits': {
+        'feddf': {
+            -1: {
+                'communication_epoch': 40,
+                'local_lr': 0.001,
+                'public_lr': 0.001,
+                'local_epoch': 20,
+                'public_epoch': 1,
+                'public_batch_size': 256,
+                'local_batch_size': 128,
+            },
+        },
+        'fccl': {
+            -1: {
+                'communication_epoch': 40,
+                'local_lr': 0.001,
+                'public_lr': 0.001,
+                'local_epoch': 20,
+                'public_epoch': 1,
+                'public_batch_size': 256,
+                'local_batch_size': 128,
+                'off_diag_weight': 0.0051,
 
+            },
+        },
+        'fcclplus': {
+            -1: {
+                'communication_epoch': 40,
+                'local_lr': 0.001,
+                'public_lr': 0.001,
+                'local_epoch': 20,
+                'public_epoch': 1,
+                'public_batch_size': 256,
+                'local_batch_size': 128,
+                'temp': 0.02,
+                'dis_power': 3,
+                'local_dis_power': 3
+            },
+        },
         'fedavg': {
                 'local_lr': 0.01,
                 'local_batch_size': 64,
@@ -25,7 +62,44 @@ best_args = {
         }
     },
     'fl_officecaltech': {
+        'feddf': {
+            -1: {
+                'communication_epoch': 40,
+                'local_lr': 0.001,
+                'public_lr': 0.001,
+                'local_epoch': 20,
+                'public_epoch': 1,
+                'public_batch_size': 256,
+                'local_batch_size': 128,
+            },
+        },
+        'fccl': {
+            -1: {
+                'communication_epoch': 40,
+                'local_lr': 0.001,
+                'public_lr': 0.001,
+                'local_epoch': 20,
+                'public_epoch': 1,
+                'public_batch_size': 256,
+                'local_batch_size': 128,
+                'off_diag_weight': 0.0051,
 
+            },
+        },
+        'fcclplus': {
+            -1: {
+                'communication_epoch': 40,
+                'local_lr': 0.001,
+                'public_lr': 0.001,
+                'local_epoch': 20,
+                'public_epoch': 1,
+                'public_batch_size': 256,
+                'local_batch_size': 128,
+                'temp': 0.02,
+                'dis_power': 3,
+                'local_dis_power': 3
+            },
+        },
         'fedavg': {
             'local_lr': 0.01,
             'local_batch_size': 64,
@@ -48,53 +122,7 @@ best_args = {
             'Note': '+ MSE'
         }
     },
-    'fl_office31': {
-        'fedavg': {
-            'local_lr': 0.01,
-            'local_batch_size': 64,
-        },
-        'fedprox': {
-            'local_lr': 0.01,
-            'mu': 0.01,
-            'local_batch_size': 64,
-        },
-        'moon': {
-            'local_lr': 0.01,
-            'local_batch_size': 64,
-            'temperature': 0.5,
-            'mu': 5
-        },
+    
 
-        'fpl': {
-            'local_lr': 0.01,
-            'local_batch_size': 64,
-            'Note': '+ MSE'
-        }
-
-    },
-    'fl_officehome': {
-        'fedavg': {
-            'local_lr': 0.01,
-            'local_batch_size': 64,
-        },
-        'fedprox': {
-            'local_lr': 0.01,
-            'mu': 0.01,
-            'local_batch_size': 64,
-        },
-        'moon': {
-            'local_lr': 0.01,
-            'local_batch_size': 64,
-            'temperature': 0.5,
-            'mu': 5
-        },
-
-        'fpl': {
-            'local_lr': 0.01,
-            'local_batch_size': 64,
-            'Note': '+ MSE'
-        }
-
-
-    }
+    
 }
