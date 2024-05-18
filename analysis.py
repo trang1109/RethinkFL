d6a98c035bd3a01005d67a9f268b39d02e108d60
Huyen Trang <bhtrang1109@gmail.com>
2024-05-18 17:38:41 +0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 path = './data/'
 scenarios_list = ['fl_digits', 'fl_officecaltech']
 pri_aug = 'weak'  # weak, strong
-column_mean_acc_list = ['method', 'paragroup'] + ['epoch' + str(i) for i in range(40)] + ['MEAN', 'Last_Mean']
+column_mean_acc_list = ['method', 'paragroup', 'MEAN']
 domain_list = ['inter']
 
 def load_mean_acc_list(scenario_path, domain):
@@ -22,19 +22,14 @@ def load_mean_acc_list(scenario_path, domain):
                     args_pd = pd.read_table(args_path, sep=",")
                     args_pri_aug = args_pd['pri_aug'][0]
                     if args_pri_aug == pri_aug:
-                        data_filename = f'mean_acc.csv'
+                        data_filename = 'mean_acc.csv'
                         data_path = os.path.join(para_path, data_filename)
                         if os.path.exists(data_path):
                             data = pd.read_table(data_path, sep=",")
-                            data = data.loc[:, data.columns]
                             acc_value = data.values
-                            mean_acc_value = np.mean(acc_value, axis=0)
-                            mean_acc_value = mean_acc_value.tolist()
-                            mean_acc_value = [round(item, 2) for item in mean_acc_value]
-                            last_acc_value = mean_acc_value[-3:]
-                            last_acc_value = np.mean(last_acc_value)
-                            mean_acc_value.append(round(last_acc_value, 3))
-                            acc_dict[experiment_index] = [model, para] + mean_acc_value
+                            mean_acc_value = np.mean(acc_value, axis=0)  # Mean of each column
+                            overall_mean = np.mean(mean_acc_value)  # Overall mean of all means
+                            acc_dict[experiment_index] = [model, para, round(overall_mean, 3)]
                             experiment_index += 1
     return acc_dict
 
@@ -54,11 +49,7 @@ def main():
         for _, domain in enumerate(domain_list):
             print(f'Scenario: {scenario} Domain: {domain} Augmentation: {pri_aug}')
             mean_acc_dict = load_mean_acc_list(scenario_path, domain)
-            mean_df = pd.DataFrame.from_dict(mean_acc_dict, orient='index')
-            if len(mean_df.columns) == len(column_mean_acc_list):
-                mean_df.columns = column_mean_acc_list
-            else:
-                print(f"Column count mismatch: DataFrame has {len(mean_df.columns)} columns, but {len(column_mean_acc_list)} labels provided.")
+            mean_df = pd.DataFrame.from_dict(mean_acc_dict, orient='index', columns=column_mean_acc_list)
             print(mean_df)
             output_file_path = os.path.join(scenario_path, domain + '_output.xlsx')
             mean_df.to_excel(output_file_path, na_rep='True')
