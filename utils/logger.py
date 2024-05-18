e6c5badb1a7ae46fb2a7f8b69179c45ffc384d26
Huyen Trang <bhtrang1109@gmail.com>
2024-05-18 17:46:54 +0700

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -3,7 +3,6 @@ import os
 import csv
 from utils.conf import base_path
 from utils.util import create_if_not_exists
-# 'pub_aug','public_len','public_dataset',
 useless_args = ['structure','dataset', 'model', 'csv_log', 'device_id', 'seed',
                 'tensorboard','conf_jobnum','conf_timestamp','conf_host']
 import pickle
